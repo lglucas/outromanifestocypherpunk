2452bd1822f0b827a2baddc0ed0e5faf85f924a1
lglucas <contato@lucasgalvao.com.br>
2026-06-27 16:54:29 -0300

    feat(visitor-data): feature barrel

---
+++ src/features/visitor-data/index.ts
@@ -0,0 +1,8 @@
+export type { GeoData, DeviceData, ServerVisitorData } from './types';
+export { parseUserAgent } from './parse-user-agent';
+export { formatGeo } from './format-geo';
+export { geoLookup } from './geo-lookup';
+export { classifyBattery } from './classify-battery';
+export type { BatteryState } from './classify-battery';
+export { collectFingerprint } from './client-fingerprint';
+export type { ClientFingerprint } from './client-fingerprint';
